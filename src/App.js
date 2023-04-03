5015e2445766bd17fbc3cd208f22ae1f5bb1a997
Guilherme Shibuya <73371141+guilhermeshibuya@users.noreply.github.com>
2023-04-02 22:14:01 -0300

    Add media query for mobile devices. Project deployed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import db from "./config/firebase";
 import './App.css';
 
 function App() {
+  let maxHeight;
+
+  if (window.innerHeight <= 800) {
+    maxHeight = window.innerHeight;
+  }
+
   const [videos, setVideos] = useState([]);
 
   const getVideos = async () => {
@@ -19,7 +25,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="App" style={{ maxHeight: maxHeight + "px" }}>
       <div className="app__videos">
         { videos.map(video => {
             return (
